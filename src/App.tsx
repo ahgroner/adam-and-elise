dcac31bf59542aab35fa0f9d5cbe3f62279b230a
Adam Groner <adamhgroner@gmail.com>
2023-08-31 08:42:02 +0000

    lowercase password check

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const App = () => {
   const [password, setPassword] = useState<string>('');
 
   const checkPassword = () => {
-    if (password === PASSWORD) {
+    if (password.toLowerCase() === PASSWORD.toLowerCase()) {
       setAuthCookie();
       setAuthed(true);
     }
